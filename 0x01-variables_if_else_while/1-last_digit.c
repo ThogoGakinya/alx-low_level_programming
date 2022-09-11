380390f8bdaccb5d20284c6a884257a3acb836d3
ThogoGakinya <samsongakinya3@gail.com>
2022-09-11 09:55:00 -0700

    1-last_digit.c-2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int l;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 l = n % 10;
-if (l > 5)   
+if (l > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
@@ -26,7 +26,7 @@ printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);      
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
-return (0); 
+return (0);
 }
