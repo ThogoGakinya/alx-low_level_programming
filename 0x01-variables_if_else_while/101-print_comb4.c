492dc6f50275ef99172c7de75eba4352efcca5a0
ThogoGakinya <samsongakinya3@gail.com>
2022-09-12 11:17:24 -0700

    101-print_comb4.c-3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,13 +15,13 @@ while (c <= 999)
 f_d = (c / 100 + '0');
 m_d = (c / 10 % 10 + '0');
 l_d = (c % 10 + '0');
-if ((f_d < m_d) && (m_d < l_d))
+if ((f_d < m_d)&&(m_d < l_d))
 {
-putchar(f_d); 
+putchar(f_d);
 putchar(m_d);
 putchar(l_d);
 if (c != 789)
-{ 
+{
 putchar(',');
 putchar(' ');
 }
