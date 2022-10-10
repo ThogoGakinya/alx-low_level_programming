1b6b8bd4edcc8f79f699d281e09de0a3cd726c4d
SAMSON GAKINYA <112089526+ThogoGakinya@users.noreply.github.com>
2022-10-10 17:57:16 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,9 +21,7 @@ while (s2[x] != '\0')
 {
 x++;
 }
-
-/*getting memory size of the new concatinated string*/
-s = malloc((i *sizeof(char)) + ((x + 1 *sizeof(char))));
+s = malloc((i * sizeof(char)) + ((x + 1 * sizeof(char))));
 if (s == NULL)
 {
 return (NULL);
