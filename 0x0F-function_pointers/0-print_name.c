6bbf56df796c387158c88e789388820e1f44957e
SAMSON GAKINYA <112089526+ThogoGakinya@users.noreply.github.com>
2022-10-19 22:10:47 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,19 +2,14 @@
 #include "function_pointers.h"
 
 /**
-* print_name - prints a name as is
-* @name: name of the person
-* @f: is pointer
-* Return: Nothing.
-*/
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-{
-return;
-}
-else
-{
-f(name);
-}
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
