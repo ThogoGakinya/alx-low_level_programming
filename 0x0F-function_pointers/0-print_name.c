dcc1a53fa14e67e8eae687a7227d427ddb554151
SAMSON GAKINYA <112089526+ThogoGakinya@users.noreply.github.com>
2022-10-19 22:40:25 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,19 +1,15 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
 /**
-* print_name - prints a name as is
-* @name: name of the person
-* @f: is pointer
-* Return: Nothing.
-**/
+ * print_name - f points to either func in main
+ * @name: name
+ * @f: pointer to print upp or non upp func
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-{
-return;
-}
-else
-{
-f(name);
-}
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
